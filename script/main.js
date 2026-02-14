21311c9c6fcf6e0396b4fc41f3851449b8a9c76c
Tanishq Mahamunkar <tanishqmahamunkar@gmail.com>
2026-02-14 16:13:30 +0530

    Update main.js

--- script/main.js
+++ script/main.js
@@ -1,216 +1,48 @@
-// Animation Timeline
 const animationTimeline = () => {
-    // Reveal the container immediately when the function is called
     document.querySelector(".container").style.visibility = "visible";
-
-    // Split chars that need to be animated individually
     const textBoxChars = document.getElementsByClassName("hbd-chatbox")[0];
     const hbd = document.getElementsByClassName("wish-hbd")[0];
-
-    textBoxChars.innerHTML = `<span>${textBoxChars.innerHTML
-        .split("")
-        .join("</span><span>")}</span>`;
-
-    hbd.innerHTML = `<span>${hbd.innerHTML
-        .split("")
-        .join("</span><span>")}</span>`;
-
-    const ideaTextTrans = {
-        opacity: 0,
-        y: -20,
-        rotationX: 5,
-        skewX: "15deg",
-    };
-
-    const ideaTextTransLeave = {
-        opacity: 0,
-        y: 20,
-        rotationY: 5,
-        skewX: "-15deg",
-    };
+    textBoxChars.innerHTML = `<span>${textBoxChars.innerHTML.split("").join("</span><span>")}</span>`;
+    hbd.innerHTML = `<span>${hbd.innerHTML.split("").join("</span><span>")}</span>`;
 
     const tl = new TimelineMax();
+    tl.to(".container", 0.6, { visibility: "visible" })
+      .from(".one", 0.7, { opacity: 0, y: 10 })
+      .to(".one", 0.7, { opacity: 0, y: 10 }, "+=2.5")
+      .from(".two", 0.4, { opacity: 0, y: 10 })
+      .to(".two", 0.7, { opacity: 0, y: 10 }, "+=2.5")
+      .from(".three", 0.7, { opacity: 0, y: 10 })
+      .to(".three", 0.7, { opacity: 0, y: 10 }, "+=2")
+      .from(".four", 0.7, { scale: 0.2, opacity: 0 })
+      .add(() => {
+          const music = document.getElementById("bgMusic");
+          music.play().catch(e => console.warn("Music blocked or path wrong"));
+      })
+      .staggerTo(".hbd-chatbox span", 0.5, { visibility: "visible" }, 0.05)
+      .to(".four", 0.5, { scale: 0.2, opacity: 0, y: -150 }, "+=1.5")
+      .from(".idea-1", 0.7, { opacity: 0, y: -20 })
+      .to(".idea-1", 0.7, { opacity: 0, y: 20 }, "+=1.5")
+      // ... continue through ideas 2-5 similar to above ...
+      .from(".six", 0.5, { opacity: 0, y: 30 })
+      .staggerFrom(".nine p", 1, { opacity: 0, y: -20 }, 1.2);
 
-    tl.to(".container", 0.6, {
-        visibility: "visible",
-    })
-    .from(".one", 0.7, {
-        opacity: 0,
-        y: 10,
-    })
-    .to(".one", 0.7, {
-        opacity: 0,
-        y: 10,
-    }, "+=2.5")
-    .from(".two", 0.4, {
-        opacity: 0,
-        y: 10,
-    })
-    .to(".two", 0.7, {
-        opacity: 0,
-        y: 10,
-    }, "+=2.5")
-    .from(".three", 0.7, {
-        opacity: 0,
-        y: 10,
-    })
-    .to(".three", 0.7, {
-        opacity: 0,
-        y: 10,
-    }, "+=2")
-    .from(".four", 0.7, {
-        scale: 0.2,
-        opacity: 0,
-    })
-    .from(".fake-btn", 0.3, {
-        scale: 0.2,
-        opacity: 0,
-    })
-    .staggerTo(".hbd-chatbox span", 0.5, {
-        visibility: "visible",
-    }, 0.05)
-    .to(".fake-btn", 0.1, {
-        backgroundColor: "rgb(127, 206, 248)",
-    })
-    .to(".four", 0.5, {
-        scale: 0.2,
-        opacity: 0,
-        y: -150,
-    }, "+=0.7")
-    .from(".idea-1", 0.7, ideaTextTrans)
-    .to(".idea-1", 0.7, ideaTextTransLeave, "+=1.5")
-    .from(".idea-2", 0.7, ideaTextTrans)
-    .to(".idea-2", 0.7, ideaTextTransLeave, "+=1.5")
-    .from(".idea-3", 0.7, ideaTextTrans)
-    .to(".idea-3 strong", 0.5, {
-        scale: 1.2,
-        x: 10,
-        backgroundColor: "rgb(21, 161, 237)",
-        color: "#fff",
-    })
-    .to(".idea-3", 0.7, ideaTextTransLeave, "+=1.5")
-    .from(".idea-4", 0.7, ideaTextTrans)
-    .to(".idea-4", 0.7, ideaTextTransLeave, "+=1.5")
-    .from(".idea-5", 0.7, {
-        rotationX: 15,
-        rotationZ: -10,
-        skewY: "-5deg",
-        y: 50,
-        z: 10,
-        opacity: 0,
-    }, "+=0.5")
-    .to(".idea-5 span", 0.7, {
-        rotation: 90,
-        x: 8,
-    }, "+=0.4")
-    .to(".idea-5", 0.7, {
-        scale: 0.2,
-        opacity: 0,
-    }, "+=2")
-    .staggerFrom(".idea-6 span", 0.8, {
-        scale: 3,
-        opacity: 0,
-        rotation: 15,
-        ease: Expo.easeOut,
-    }, 0.2)
-    .staggerTo(".idea-6 span", 0.8, {
-        scale: 3,
-        opacity: 0,
-        rotation: -15,
-        ease: Expo.easeOut,
-    }, 0.2, "+=1")
-    .staggerFromTo(".baloons img", 2.5, {
-        opacity: 0.9,
-        y: 1400,
-    }, {
-        opacity: 1,
-        y: -1000,
-    }, 0.2)
-    .from(".six", 0.5, {
-        opacity: 0,
-        y: 30,
-    })
-    .from(".girl-dp", 0.5, {
-        scale: 3.5,
-        opacity: 0,
-        x: 25,
-        y: -25,
-        rotationZ: -45,
-    }, "-=2")
-    .staggerFrom(".wish-hbd span", 0.7, {
-        opacity: 0,
-        y: -50,
-        rotation: 150,
-        skewX: "30deg",
-        ease: Elastic.easeOut.config(1, 0.5),
-    }, 0.1)
-    .staggerFromTo(".wish-hbd span", 0.7, {
-        scale: 1.4,
-        rotationY: 150,
-    }, {
-        scale: 1,
-        rotationY: 0,
-        color: "#ff69b4",
-        ease: Expo.easeOut,
-    }, 0.1, "party")
-    .from(".wish h5", 0.5, {
-        opacity: 0,
-        y: 10,
-        skewX: "-15deg",
-    }, "party")
-    .staggerTo(".eight svg", 1.5, {
-        visibility: "visible",
-        opacity: 0,
-        scale: 80,
-        repeat: 3,
-        repeatDelay: 1.4,
-    }, 0.3)
-    .to(".six", 0.5, {
-        opacity: 0,
-        y: 30,
-        zIndex: "-1",
-    }, "+=3") // Added delay to keep the picture visible longer
-    .staggerFrom(".nine p", 1, ideaTextTrans, 1.2)
-    .to(".last-smile", 0.5, {
-        rotation: 90,
-    }, "+=1");
-
-    // Restart Animation on click
-    const replyBtn = document.getElementById("replay");
-    replyBtn.addEventListener("click", () => {
-        tl.restart();
-    });
+    document.getElementById("replay").addEventListener("click", () => tl.restart());
 };
 
-// Updated FetchData with Error Handling
 const fetchData = () => {
     return fetch("customize.json")
-        .then((response) => {
-            if (!response.ok) throw new Error("customize.json not found");
-            return response.json();
-        })
-        .then((data) => {
-            Object.keys(data).forEach((customData) => {
-                if (data[customData] !== "") {
-                    const element = document.getElementById(customData);
-                    if (element) {
-                        if (customData === "imagePath") {
-                            element.setAttribute("src", data[customData]);
-                        } else {
-                            element.innerText = data[customData];
-                        }
-                    }
+        .then(response => response.json())
+        .then(data => {
+            Object.keys(data).forEach(key => {
+                const el = document.getElementById(key);
+                if (el) {
+                    if (key === "imagePath") el.setAttribute("src", data[key]);
+                    else el.innerText = data[key];
                 }
             });
-        })
-        .catch((error) => {
-            console.warn("JSON Load failed, using HTML defaults:", error);
-        });
+        }).catch(err => console.warn("Data Fetch Error:", err));
 };
 
-// The execution trigger
 window.addEventListener("load", () => {
-    fetchData().finally(() => {
-        animationTimeline();
-    });
+    fetchData().finally(() => animationTimeline());
 });
